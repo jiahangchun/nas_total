51f3a2c0dfddb9055fb774d0403b1464f5f8e83e
jiahangchun <jiahangchun@163.com>
2024-04-27 21:49:20 +0800

    测试 免密

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@ echo "开始执行部署程序"
 export CONFIG=/home/jiahangchun/jiahangchun_config
 export DATA=/home/jiahangchun/jiahangchun_data
 
-sudo chmod -R 777 $CONFIG
-sudo echo $CONFIG
-sudo chmod -R 777 $DATA
-sudo echo $DATA
+chmod -R 777 $CONFIG
+echo $CONFIG
+chmod -R 777 $DATA
+echo $DATA
 
 
-sudo docker-compose -f ./compose.yaml up -d
+docker-compose -f ./compose.yaml up -d
