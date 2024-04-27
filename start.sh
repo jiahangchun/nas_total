9f12de1b6085295eff73a4c456a59b37cff07dd2
jiahangchun <jiahangchun@163.com>
2024-04-27 22:52:19 +0800

    测试 免密

--- start.sh
+++ start.sh
@@ -1,7 +1,13 @@
-#!/bin/bash
-echo "用户""$USER""开始部署docker"
-sudo -S su
 export CONFIG=/home/jiahangchun/jiahangchun_config
 export DATA=/home/jiahangchun/jiahangchun_data
 
+
+
+echo "对文件夹赋值"
+chmod -R 777 $CONFIG
+echo "配置地址："$CONFIG
+chmod -R 777 $DATA
+echo "数据地址："$DATA
+
+echo "用户""$USER""开始部署docker"
 docker-compose -f ./compose.yaml up -d
