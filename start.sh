efce1071d6ab69521f51974b1086f78a974f1e7a
jiahangchun <jiahangchun@163.com>
2024-04-27 21:58:04 +0800

    测试 免密

--- start.sh
+++ start.sh
@@ -4,10 +4,10 @@ echo "开始执行部署程序"
 export CONFIG=/home/jiahangchun/jiahangchun_config
 export DATA=/home/jiahangchun/jiahangchun_data
 
-sudo chmod -R 777 $CONFIG
-sudo echo $CONFIG
-sudo chmod -R 777 $DATA
-sudo echo $DATA
+sudo -S chmod -R 777 $CONFIG
+sudo -S echo $CONFIG
+sudo -S chmod -R 777 $DATA
+sudo -S echo $DATA
 
 echo "对文件夹赋值"
 sudo docker-compose -f ./compose.yaml up -d
