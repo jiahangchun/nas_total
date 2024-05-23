8237b1f2c3a30688b7fcdc24d1352f7fab3123c8
贾杭春 <jiahangchun@moredian.com>
2024-05-23 19:47:33 +0800

    关闭mysql 数据库

--- start.sh
+++ start.sh
@@ -10,11 +10,11 @@ echo "配置地址："$CONFIG
 chmod -R 777 $DATA
 echo "数据地址："$DATA
 
-## shellcheck disable=SC2046
-#docker stop $(docker ps -a -q)
-## shellcheck disable=SC2046
-#docker  rm $(docker ps -a -q)
-#echo "删除所有容器"
+# shellcheck disable=SC2046
+docker stop $(docker ps -a -q)
+# shellcheck disable=SC2046
+docker  rm $(docker ps -a -q)
+echo "删除所有容器"
 
 echo "用户 ""$USER"" 开始部署docker"
 docker-compose -f ./compose.yaml --env-file=$CONFIG/.env up -d
