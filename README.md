1036c87b3e13f77198f8eabf255e1e27163c4871
jiahangchun <jiahangchun@163.com>
2024-05-25 00:48:24 +0800

    适配 windows

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # ai 文档
 
+## 执行命令
+cd /c/jiahangchun/idea/code/nas_total
+./start.sh
+
+
+## 改switch hosts 文件
+127.0.0.1 whoami.jiahangchun.icu
+
+## 验证
+访问 http://whoami.jiahangchun.icu:12001/
+
+
 
 
 
--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
 # shellcheck disable=SC2232
-export CONFIG=/home/jiahangchun/jiahangchun_config
-export DATA=/home/jiahangchun/jiahangchun_data
-
-
+export CONFIG=C://jiahangchun//idea//code//docker//config
+export DATA=C://jiahangchun//idea//code//docker//data
 
 echo "对文件夹赋值"
 chmod -R 777 $CONFIG
@@ -10,12 +8,7 @@ echo "配置地址："$CONFIG
 chmod -R 777 $DATA
 echo "数据地址："$DATA
 
-# shellcheck disable=SC2046
-docker stop $(docker ps -a -q)
-# shellcheck disable=SC2046
-docker  rm $(docker ps -a -q)
-echo "删除所有容器"
 
 echo "用户 ""$USER"" 开始部署docker"
-docker-compose -f ./compose.yaml --env-file=$CONFIG/.env up -d
+docker-compose -f ./compose.yaml --env-file=$CONFIG//env.txt up -d
 exit;
