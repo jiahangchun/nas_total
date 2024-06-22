3ca0e2e230464b5196495dc2311e9a5df2d96599
贾杭春 <jiahangchun@moredian.com>
2024-06-22 16:42:13 +0800

    配置文件

--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@ export CONFIG=/storefile/config
 export DATA=/storefile/data
 
 # 配置文件
-mdkir $CONFIG
-mdkir $DATA
+mkdir $CONFIG
+mkdir $DATA
 touch $CONFIG/.env
 
 echo "对文件夹赋值"
@@ -14,10 +14,10 @@ chmod -R 777 $DATA
 echo "数据地址："$DATA
 
 # shellcheck disable=SC2046
-docker stop $(docker ps -a -q)
+#docker stop $(docker ps -a -q)
 # shellcheck disable=SC2046
-docker  rm $(docker ps -a -q)
-echo "删除所有容器"
+#docker  rm $(docker ps -a -q)
+#echo "删除所有容器"
 
 echo "用户 ""$USER"" 开始部署docker"
 docker-compose -f ./compose.yaml --env-file=$CONFIG/env up -d
