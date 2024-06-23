4601a438c433fb7011c31cdf67c30d6a185213b6
jiahangchun <jiahangchun@163.com>
2024-06-23 14:59:03 +0800

    nas mysql

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 # shellcheck disable=SC2046
-#docker stop $(docker ps -a -q)
+#sudo docker stop $(sudo docker ps -a -q)
 # shellcheck disable=SC2046
-#docker  rm $(docker ps -a -q)
+#sudo docker  rm $(sudo docker ps -a -q)
 #echo "删除所有容器"
 
 echo "用户 ""$USER"" 开始部署docker"
